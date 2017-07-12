a8d497e241b808235efb6aa5c61e84a019cf05f0
Sinuhe Guerra Hernandez <sinuhe.guerra@summitworks.com>
2017-07-12 16:31:54 -0400

    changed to remote

---
+++ app/models/mongodbauth.js
@@ -0,0 +1,4 @@
+db.createUser({user: "todouser",pwd: "todouser123",roles:[{"role" : "readWrite","db": "todosdb"}]})
+
+//to connect from putty
+//sudo mongo admin -u sinuhe -p sinuhe
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://sinuhe:sinuhe@ec2-34-227-26-7.compute-1.amazonaws.com:27017/todosdb',
+    remoteUrl : 'mongodb://todouser:todouser123@ec2-34-227-26-7.compute-1.amazonaws.com/todosdb',
     localUrl: 'mongodb://localhost/todosdb'
 };
