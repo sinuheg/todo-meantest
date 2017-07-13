9a8dda8cdab444274f3a0ef63fab8b18ee472bd3
Sinuhe Guerra Hernandez <sinuhe.guerra@summitworks.com>
2017-07-13 11:00:52 -0400

    restart ec2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://todouser:todouser123@ec2-34-227-26-7.compute-1.amazonaws.com/todosdb',
+    remoteUrl : 'mongodb://todouser:todouser123@ec2-34-203-188-157.compute-1.amazonaws.com/todosdb',
     localUrl: 'mongodb://localhost/todosdb'
 };
